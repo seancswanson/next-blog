5fa7424979b3a1a0d23d19c5a2e4ff67884bc237
Sean Swanson <sean.swanson@dreambox.com>
2022-09-25 16:02:27 -0700

    Added utils css module. Changed layouts to include profile picture and introduction.

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,72 @@
-import styles from './layout.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
-export default function Layout({ children }) {
-    return <div className={styles.container}>{children}</div>;
+const name = 'Sean Swanson';
+export const siteTitle = 'Sean Swanson - First Next.js Blog';
+
+export default function Layout({ children, home }) {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="description"
+                    content="Personal blog of Sean Swanson, a web developer and artist based in Seattle, WA."
+                />
+                <meta
+                    property="og:image"
+                    content={`${encodeURI(
+                        '/images/profile.png')}`}
+                />
+                <meta name="og:title" content={siteTitle} />
+                <meta name="twitter:card" content="summary_large_image" />
+            </Head>
+            <header className={styles.header}>
+                {home ? (
+                    <>
+                        <Image
+                            priority
+                            src="/images/profile.png"
+                            className={utilStyles.borderCircle}
+                            height={144}
+                            width={144}
+                            alt=""
+                        />
+                        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+                    </>
+                ) : (
+                    <>
+                        <Link href="/">
+                            <a>
+                                <Image
+                                    priority
+                                    src="/images/profile.png"
+                                    className={utilStyles.borderCircle}
+                                    height={108}
+                                    width={108}
+                                    alt=""
+                                />
+                            </a>
+                        </Link>
+                        <h2 className={utilStyles.headingLg}>
+                            <Link href="/">
+                                <a className={utilStyles.colorInherit}>{name}</a>
+                            </Link>
+                        </h2>
+                    </>
+                )}
+            </header>
+            <main>{children}</main>
+            {!home && (
+                <div className={styles.backToHome}>
+                    <Link href="/">
+                        <a>← Back to home</a>
+                    </Link>
+                </div>
+            )}
+        </div>
+    );
 }
