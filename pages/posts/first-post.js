b3f7282e5494f2c694101ed86c6b54f3ed460563
Sean Swanson <sean.swanson@dreambox.com>
2022-09-25 12:37:32 -0700

    Removed contrived fb Script component.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,5 @@
 import Head from "next/head"
 import Link from "next/link"
-import Script from "next/script"
 
 export default function FirstPost() {
     return (
@@ -8,15 +7,7 @@ export default function FirstPost() {
             <Head>
                 <title>First Post</title>
             </Head>
-
-            <Script
-                src="https://connect.facebook.net/en_US/sdk.js"
-                strategy="lazyOnload"
-                onLoad={() =>
-                    console.log(`script loaded correctly, window.FB has been populated`)
-                }
-                />
-
+            <h1>First Post</h1>
             <div>
                 <h1>First Post- hell0!</h1>
                 <Link href="/">Back to home</Link>
