8fc43cf5f1a55a53372165d56eeac0aca4678489
Sean Swanson <sean.swanson@dreambox.com>
2022-09-25 14:39:36 -0700

    Added layout styling component and a global css file.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,17 +1,17 @@
-import Head from "next/head"
-import Link from "next/link"
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/Layout";
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Post</title>
             </Head>
-            <h1>First Post</h1>
             <div>
                 <h1>First Post- hell0!</h1>
                 <Link href="/">Back to home</Link>
             </div>
-        </>
+        </Layout>
     );
 }
